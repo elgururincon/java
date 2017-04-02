e8f8f5fee7526605c7060e0b153aef468e8662b0
elgururincon <davidrincont@gmail.com>
2017-04-02 18:38:00 -0500

    Create 1007.java

---
+++ 1007.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        int A, B, C, D, DIFERENCA;
+        Scanner input = new Scanner(System.in);
+        A = input.nextInt();
+        B = input.nextInt();
+        C = input.nextInt();
+        D = input.nextInt();
+        DIFERENCA = (A*B)-(C*D);
+        System.out.println("DIFERENCA = " + DIFERENCA);
+ 
+      
+    }
+ 
+}
